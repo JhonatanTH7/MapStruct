defbf36a82408db8b9af195f2d3d6cba17f3fab4
JhonatanTH7 <148970615+JhonatanTH7@users.noreply.github.com>
2024-06-20 08:55:47 -0500

    initial structure

---
+++ src/test/java/com/MapStruct/MapStructLearning/MapStructLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MapStruct.MapStructLearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapStructLearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
